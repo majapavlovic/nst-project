54d257b5a2508d78d6839d08be21910a5295dc48
majapavlovic <maja.pavlovic2703@gmail.com>
2024-08-13 22:47:43 +0200

    first commit

---
+++ src/main/java/fon/nstproject/NstprojectApplication.java
@@ -0,0 +1,13 @@
+package fon.nstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NstprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fon/nstproject/NstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package fon.nstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
