5390b63acbfd1a861376b3ee54c12003575da1b1
majapavlovic <maja.pavlovic2703@gmail.com>
2024-09-08 18:04:42 +0200

    nstprojectapplication tests

--- src/test/java/fon/nstproject/NstprojectApplicationTests.java
+++ src/test/java/fon/nstproject/NstprojectApplicationTests.java
@@ -3,7 +3,7 @@ package fon.nstproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = NstprojectApplication.class)     
 class NstprojectApplicationTests {
 
 	@Test
